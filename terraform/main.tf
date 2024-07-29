0f6ddca60eb37e2cd654475c47a64c0adc0995ff
jc-22 <juan.c.lima@live.com>
2024-07-29 03:10:33 -0300

    correcting name iam profile 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "server" {
   ami                    = "ami-01a38093d387a7497"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
+  key_name               = aws_key_pair.whodeploy.key_name
   vpc_security_group_ids = [ aws_security_group.maingroup.id ]
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
   connection {
